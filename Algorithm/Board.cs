706107598fbdd1bb52776da9c80e2637ff6d2b51
bidulgicode <pkmon1999@gmail.com>
2020-06-03 23:24:34 +0900

    init

---
+++ Algorithm/Board.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+
+        public TileType[,] _tile;
+        public int _size;
+
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size)
+        {
+            if (size % 2 == 0)
+                return;
+
+            _tile = new TileType[size, size];
+            _size = size;
+
+            // Make Mazes
+            GenetareByBinaryTree();
+        }
+
+        private void GenetareByBinaryTree()
+        {
+
+            // 길을 다 막고
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 뚫어
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue; // 기존에 막았던 부분은 패스
+
+                    if (y == _size - 2 && x == _size - 2) // 마지막 점
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0) // 0~1
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAP_SIZE = 25;
+            const int WAIT_TICK = 1000 / 30;
+            
+            Board board = new Board();
+            board.Initialize(MAP_SIZE);
+
+            Console.CursorVisible = false;
+
+            int lastTick = 0;
+            while (true)
+            {
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+
+                // 입력 
+                // 로직
+                // 랜더
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+
+            }
+        }
+    }
+}
