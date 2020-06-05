3e0131662e29219623db3c049f576a8026fcd083
bidulgicode <pkmon1999@gmail.com>
2020-06-05 16:02:44 +0900

    그래프 실습용 신규 프로젝트 추가

---
+++ Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    class Program
+    {
+        class Graph
+        {
+            int[,] adj = new int[6, 6]
+            {
+                { 0, 1, 0, 1, 0, 0 },
+                { 1, 0, 1, 1, 0, 0 },
+                { 0, 1, 0, 0, 0, 0 },
+                { 1, 1, 0, 0, 1, 0 },
+                { 0, 0, 0, 1, 0, 1 },
+                { 0, 0, 0, 0, 1, 0 },
+            };
+
+            List<int>[] adj2 = new List<int>[]
+            {
+                new List<int>() { 1, 3 },
+                new List<int>() { 0, 2, 3 },
+                new List<int>() { 1 },
+                new List<int>() { 0, 1, 4 },
+                new List<int>() { 3, 5 },
+                new List<int>() { 4 },
+            };
+        }
+        // 스택 : LIFO
+        // 큐 : FIFO
+        static void Main(string[] args)
+        {
+            // DFS 깊이우선
+            
+            // BFS 너비우선
+        }
+    }
+}
